df8f0710167baeff1162650102158e10c6130528
mensafri <safrijawad1245@gmail.com>
2023-04-21 08:15:11 +0700

    belajar dulu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const drumPads = [
+    {
+      keyId: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyId: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyId: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyId: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyId: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyId: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyId: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyId: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyId: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  const play = (val) => {
+    new Audio(val.src).play()
+  }
+
+  const [text, setText] = useState('')
+
+  
+
   return (
-    <div className="App">
-      <h1>tes</h1>
+    <div id="drum-machine">
+      <div id="display">{text}</div>
+      <div className="drum-pads">
+        {drumPads.map((pad) => (
+          <button key={pad.keyId} className="drum-pad" onClick={() => {play(pad)}} id={pad.text} value='PLAY'>
+            {pad.text}
+            <audio src={pad.src} className="clip" id={pad.text}></audio>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
